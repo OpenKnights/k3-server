638a4f9b91aa6cb1138916a01f2b810510c084ea
king3 <75344974+coderking3@users.noreply.github.com>
2026-07-30 10:17:07 +0800

    Delete test-d/types.ts

--- test-d/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Routes } from '#types/routes'
-
-export const routes: Routes = { '/valid': { GET: () => 'ok' } }
-
-// @ts-expect-error - A route config must define a method or children.
-export const emptyRoutes: Routes = { '/empty': {} }
